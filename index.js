f3fa8b84dfca4b79d8d51a6ae5e38347a8c4670c
titophil <kipronotitus254@gmail.com>
2025-03-11 03:03:26 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+function receivesAFunction(callback){
+    if(typeof callback==='function')
+        return callback()
+}
+
+function returnsANamedFunction(){
+    return function namedfunction(){
+   
+}
+return (`This is a function`)
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    }
+}
+
